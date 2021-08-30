2e62f21de23ca6998b608c82e54f46b832b32e4c
Erik Storås Sommer <erikssommer@gmail.com>
2021-08-30 13:53:12 +0200

    Navigating binary tree

---
+++ src/eriksommer/week2/BinaryTree.java
@@ -0,0 +1,23 @@
+package eriksommer.week2;
+
+public class BinaryTree {
+    public int parent(int id) {
+        return id / 2;
+    }
+
+    public int sibling(int id) {
+        return parent(id) * 2 == id ? id + 1 : id - 1;
+    }
+
+    public int uncle(int id) {
+        return sibling(parent(id));
+    }
+
+    public int grandparent(int id) {
+        return parent(parent(id));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Binarytrees");
+    }
+}
