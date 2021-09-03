868797760b1c3828d5f292c09d90ec49b131bb0f
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-03 13:38:22 +0200

    Moved faculty method to Table class

--- src/eriksommer/structure/Table.java
+++ src/eriksommer/structure/Table.java
@@ -2,4 +2,12 @@ package eriksommer.structure;
 
 public class Table {
     private Table(){}
+
+    public static long fak(int a){
+        if(a <= 1) {
+            return 1;
+        }
+
+        return a * fak(a-1);
+    }
 }
