fff3fa2cf340fb2f1ad8e7494665b03db91dcd56
Erik Storås Sommer <erikssommer@gmail.com>
2021-09-03 13:41:21 +0200

    Added max, min and tournament method

--- src/eriksommer/structure/Table.java
+++ src/eriksommer/structure/Table.java
@@ -3,6 +3,45 @@ package eriksommer.structure;
 public class Table {
     private Table(){}
 
+    public static int max(int[] a, int from, int to) {
+        int maxindex = from;
+        int maxvalue = a[from];
+
+        for (int i = from + 1; i < to; i++) {
+            if (a[i] > maxvalue) {
+                maxindex = i;
+                maxvalue = a[i];
+            }
+        }
+        return maxindex;
+    }
+
+    public static int min(int[] a, int from, int to) {
+        int minindex = from;
+        int minvalue = a[from];
+
+        for (int i = from + 1; i < to; i++) {
+            if (a[i] > minvalue) {
+                minindex = i;
+                minvalue = a[i];
+            }
+        }
+        return minindex;
+    }
+
+    public static int tournament(int[] a, int begin, int end) {
+        for (int i = begin; i > 0; --i) {
+            int left = 2 * i;
+            int right = 2 * i + 1;
+            if (a[left] > a[right]) {
+                a[i] = a[left];
+            } else {
+                a[i] = a[right];
+            }
+        }
+        return a[1];
+    }
+
     public static long fak(int a){
         if(a <= 1) {
             return 1;
